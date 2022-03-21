950d7b0db92cecf47706ab8a2a84f26d67790494
Christine Banek <cbanek@gmail.com>
2022-03-21 09:43:38 -0700

    [DM-34074] Add api hook to post status
    Okay this should hopefully allow us to get the status from the
    local filesystem, and sherlock will do a PUT here to write it.

---
+++ pages/api/[environment].js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+export default function EnvironmentStatusPush(req, res) {
+  let environment = req.query["environment"];
+
+  if (req.method === "PUT") {
+    console.log(req);
+    console.log(req.body);
+    fs.writeFile("/tmp/" + environment, JSON.stringify(req.body), (error) => {
+      console.error(error);
+    });
+    res.status(200).json({ status: "ok" });
+  } else if (req.method == "GET") {
+    fs.readFile("/tmp/" + environment, (error, data) => {
+      if (error) {
+        res.status(500).json({ error: error });
+        console.error(error);
+        return;
+      }
+      res.status(200).json(data);
+    });
+  } else {
+    // Handle any other HTTP method
+    res.status(405).json({ error: "Method not allowed" });
+  }
+}
