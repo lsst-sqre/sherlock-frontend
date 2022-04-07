216e8317eb79461c4f553c8205c569f9dbcb3a08
Christine Banek <cbanek@gmail.com>
2022-04-07 00:09:44 -0700

    [DM-34317] Gate PUT on a token
    Each environment has its own token, and the key will come from this
    JSON as a kubernetes / vault secret.  The environments.json here is
    just for testing.
    
    We also load it in a handy way into a global variable so it's from
    the filesystem as opposed to hard coded.

--- push_status.sh
+++ push_status.sh
@@ -2,4 +2,4 @@
 # Push the status to the dev server running on localhost
 # by running `npm run dev`
 
-curl -vv -X PUT http://localhost:3000/api/data -H "Authorization: token DEADKEY"
+curl -vv -X PUT http://localhost:3000/api/data -H "Authorization: token DEADKEY1"
