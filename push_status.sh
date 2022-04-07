50e9b132f8bb69fea45539727d1dd5074b111603
Christine Banek <cbanek@gmail.com>
2022-04-07 00:22:38 -0700

    [DM-34317] Update kustomize to mount environments.json
    This should let it mount environment.json from the vault secret
    as a simple file where it should be.

--- push_status.sh
+++ push_status.sh
@@ -2,4 +2,4 @@
 # Push the status to the dev server running on localhost
 # by running `npm run dev`
 
-curl -vv -X PUT http://localhost:3000/api/data -H "Authorization: token DEADKEY1"
+curl -vv -X PUT http://localhost:3000/api/data -H "Content-Type: application/json" -H "Authorization: token DEADKEY1" --data @example-status.json
